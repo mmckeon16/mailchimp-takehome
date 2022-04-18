a06f1dcee1c10ba1534f2a99988d611f3d375518
mmckeon16 <mmckeon204@gmail.com>
2022-04-18 01:51:18 -0400

    fix: color for unfocused tabs and aria errors

--- src/components/TabPanel/TabPanel.js
+++ src/components/TabPanel/TabPanel.js
@@ -6,13 +6,14 @@ import Box from "@mui/material/Box";
 export default function TabPanel(props) {
   const { children, value, index, label, ...other } = props;
 
-  const hiddenBool = !label || !value || value !== index;
+  const hiddenBool = !label || value === undefined || value !== index;
+
   return (
     <div
       role="tabpanel"
       hidden={hiddenBool}
       id={`tabpanel-${label}`}
-      aria-labelledby={`tab-${label}`}
+      aria-labelledby={label}
       {...other}
     >
       {!hiddenBool && <Box sx={{ p: 3 }}>{children}</Box>}
--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -16,7 +16,8 @@ const StyledTab = styled(props => <Tab disableRipple {...props} />)(() => ({
   },
   "&.Mui-focusVisible": {
     color: "#163E46"
-  }
+  },
+  color: "secondary.main"
 }));
 
 export default function Tabs() {
@@ -31,62 +32,87 @@ export default function Tabs() {
         width: "100vw"
       }}
     >
-      <MuiTabs
-        onChange={handleChange}
-        variant="scrollable"
-        value={value}
-        aria-label="Tabs of different locations for this Mailchimp project"
-        sx={{
-          "& .Mui-focusVisible": {
-            border: theme => `1px solid ${theme.palette.primary.main}`
-          },
-          borderBottom: "1px solid #e8e8e8"
-        }}
-        selectionFollowsFocus
-      >
-        <StyledTab label="Oakland" id="oakland" />
-        <StyledTab label="Atlanta" id="atlanta" />
-        <StyledTab label="Brooklyn" id="brooklyn" />
-        <StyledTab label="Vancouver" id="vancouver" />
-        <StyledTab label="Santa Monica" id="santa-monica" />
-      </MuiTabs>
+      <nav>
+        <MuiTabs
+          onChange={handleChange}
+          variant="scrollable"
+          value={value}
+          aria-label="Tabs of different locations for this Mailchimp project"
+          sx={{
+            "& .Mui-focusVisible": {
+              border: theme => `1px solid ${theme.palette.primary.main}`
+            },
+            borderBottom: "1px solid #e8e8e8"
+          }}
+          selectionFollowsFocus
+        >
+          <StyledTab
+            label="Oakland"
+            id="oakland"
+            aria-controls="tabpanel-oakland"
+          />
+          <StyledTab
+            label="Atlanta"
+            id="atlanta"
+            aria-controls="tabpanel-atlanta"
+          />
+          <StyledTab
+            label="Brooklyn"
+            id="brooklyn"
+            aria-controls="tabpanel-brooklyn"
+          />
+          <StyledTab
+            label="Vancouver"
+            id="vancouver"
+            aria-controls="tabpanel-vancouver"
+          />
+          <StyledTab
+            label="Santa Monica"
+            id="santa-monica"
+            aria-controls="tabpanel-santa-monica"
+          />
+        </MuiTabs>
+      </nav>
       {/* TODO make below into component */}
-      <TabPanel value={value} index={0}>
-        <Typography>
-          Cats meow for their humans, not other cats. Heres{" "}
-          <Link href="https://www.youtube.com/channel/UCheL-cUqfzUB8dfM_rFOfDQ">
-            some info
-          </Link>{" "}
-          about Jackson Galaxy to confirm.
-        </Typography>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <Typography>
-          <Link href="https://www.lipsum.com/">Lorem Ipsum</Link> is simply
-          dummy text of the printing and typesetting industry. Lorem Ipsum has
-          been the industry's standard dummy text ever since the 1500s, when an
-          unknown printer took a galley of type and scrambled it to make a type
-          specimen book. It has survived not only five centuries, but also the
-          leap into electronic typesetting, remaining essentially unchanged. It
-          was popularised in the 1960s with the release of Letraset sheets
-          containing Lorem Ipsum passages, and more recently with desktop
-          publishing software like Aldus PageMaker including versions of Lorem
-          Ipsum. <Link href="https://lifeskillssoftware.tech/">Here's</Link>{" "}
-          another link.
-        </Typography>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <Typography>
-          Just repeating this message for tab 3.{" "}
-          <Link href="https://www.lipsum.com/">Lorem Ipsum</Link> is simply
-          dummy text of the printing and typesetting industry. Lorem Ipsum has
-          been the industry's standard dummy text ever since the 1500s, when an
-          unknown printer took a galley of type and scrambled it to make a type
-          specimen book.{" "}
-          <Link href="https://lifeskillssoftware.tech/">Here's</Link> another
-          link.
-        </Typography>
-      </TabPanel>
+      <main>
+        <TabPanel value={value} index={0} label="oakland">
+          <Typography>
+            Cats meow for their humans, not other cats. Heres{" "}
+            <Link href="https://www.youtube.com/channel/UCheL-cUqfzUB8dfM_rFOfDQ">
+              some info
+            </Link>{" "}
+            about Jackson Galaxy to confirm.
+          </Typography>
+        </TabPanel>
+        <TabPanel value={value} index={1} label="atlanta">
+          <Typography>
+            <Link href="https://www.lipsum.com/">Lorem Ipsum</Link> is simply
+            dummy text of the printing and typesetting industry. Lorem Ipsum has
+            been the industry's standard dummy text ever since the 1500s, when
+            an unknown printer took a galley of type and scrambled it to make a
+            type specimen book. It has survived not only five centuries, but
+            also the leap into electronic typesetting, remaining essentially
+            unchanged. It was popularised in the 1960s with the release of
+            Letraset sheets containing Lorem Ipsum passages, and more recently
+            with desktop publishing software like Aldus PageMaker including
+            versions of Lorem Ipsum.{" "}
+            <Link href="https://lifeskillssoftware.tech/">Here's</Link> another
+            link.
+          </Typography>
+        </TabPanel>
+        <TabPanel value={value} index={2} label="brooklyn">
+          <Typography>
+            Just repeating this message for tab 3.{" "}
+            <Link href="https://www.lipsum.com/">Lorem Ipsum</Link> is simply
+            dummy text of the printing and typesetting industry. Lorem Ipsum has
+            been the industry's standard dummy text ever since the 1500s, when
+            an unknown printer took a galley of type and scrambled it to make a
+            type specimen book.{" "}
+            <Link href="https://lifeskillssoftware.tech/">Here's</Link> another
+            link.
+          </Typography>
+        </TabPanel>
+      </main>
     </Box>
   );
 }
