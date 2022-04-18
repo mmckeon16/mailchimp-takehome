80adcfc9101f97cee4ca9404adb7b8f858dd47fd
mmckeon16 <mmckeon204@gmail.com>
2022-04-18 00:13:26 -0400

    feat: adding styling for focus - TODO need to improve the styling for the focus on the entire tabs area

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 
+import { styled } from "@mui/material/styles";
 import { default as MuiTabs } from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
@@ -8,6 +9,22 @@ import Typography from "@mui/material/Typography";
 
 import TabPanel from "../TabPanel/TabPanel";
 
+const StyledTab = styled(props => <Tab disableRipple {...props} />)(
+  ({ theme }) => ({
+    textTransform: "none",
+    // fontWeight: theme.typography.fontWeightRegular,
+    // fontSize: theme.typography.pxToRem(15),
+    // marginRight: theme.spacing(1),
+    // color: "rgba(255, 255, 255, 0.7)",
+    "&.Mui-selected": {
+      color: "primary.main"
+    },
+    "&.Mui-focusVisible": {
+      color: "#163E46"
+    }
+  })
+);
+
 export default function Tabs() {
   const [value, setValue] = React.useState(0);
   const handleChange = (event, newValue) => {
@@ -15,16 +32,32 @@ export default function Tabs() {
   };
 
   return (
-    <Box sx={{ width: "100%" }}>
+    <Box
+      sx={{
+        width: "100%"
+      }}
+    >
       <MuiTabs
         onChange={handleChange}
+        variant="scrollable"
         value={value}
         aria-label="Tabs where selection follows focus"
+        sx={{
+          width: "100%",
+          borderBottom: "1px solid #e8e8e8",
+          "& .MuiTabs-indicator": {
+            backgroundColor: "primary.main"
+          },
+          "& .Mui-focusVisible": {
+            borderColor: "primary.main",
+            border: 1
+          }
+        }}
         selectionFollowsFocus
       >
-        <Tab label="Item One" />
-        <Tab label="Item Two" />
-        <Tab label="Item Three" />
+        <StyledTab label="Item One" />
+        <StyledTab label="Item Two" />
+        <StyledTab label="Item Three" />
       </MuiTabs>
       {/* TODO make below into component */}
       <TabPanel value={value} index={0}>
@@ -37,10 +70,31 @@ export default function Tabs() {
         </Typography>
       </TabPanel>
       <TabPanel value={value} index={1}>
-        <Typography>Item Two</Typography>
+        <Typography>
+          <Link href="https://www.lipsum.com/">Lorem Ipsum</Link> is simply
+          dummy text of the printing and typesetting industry. Lorem Ipsum has
+          been the industry's standard dummy text ever since the 1500s, when an
+          unknown printer took a galley of type and scrambled it to make a type
+          specimen book. It has survived not only five centuries, but also the
+          leap into electronic typesetting, remaining essentially unchanged. It
+          was popularised in the 1960s with the release of Letraset sheets
+          containing Lorem Ipsum passages, and more recently with desktop
+          publishing software like Aldus PageMaker including versions of Lorem
+          Ipsum. <Link href="https://lifeskillssoftware.tech/">Here's</Link>{" "}
+          another link.
+        </Typography>
       </TabPanel>
       <TabPanel value={value} index={2}>
-        <Typography>Item Three</Typography>
+        <Typography>
+          Just repeating this message for tab 3.{" "}
+          <Link href="https://www.lipsum.com/">Lorem Ipsum</Link> is simply
+          dummy text of the printing and typesetting industry. Lorem Ipsum has
+          been the industry's standard dummy text ever since the 1500s, when an
+          unknown printer took a galley of type and scrambled it to make a type
+          specimen book.{" "}
+          <Link href="https://lifeskillssoftware.tech/">Here's</Link> another
+          link.
+        </Typography>
       </TabPanel>
     </Box>
   );
